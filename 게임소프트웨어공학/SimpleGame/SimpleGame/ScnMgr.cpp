26f23e524084ebf0c15432aaf93095c69b9b80c1
Kim Young Bum <bumizi95@naver.com>
2019-09-19 19:00:50 +0900

    겜수, 겜소공 19일 수업
    겜수, 겜소공 19일 수업

--- 게임소프트웨어공학/SimpleGame/SimpleGame/ScnMgr.cpp
+++ 게임소프트웨어공학/SimpleGame/SimpleGame/ScnMgr.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "ScnMgr.h"
+#include "Object.h"
 
 
 ScnMgr::ScnMgr()
@@ -9,6 +10,25 @@ ScnMgr::ScnMgr()
 	if (!m_Renderer->IsInitialized())
 	{
 		std::cout << "Renderer could not be initialized.. \n";
+	}
+
+	//Init Test Object
+	m_TestObject = new Object();
+	m_TestObject->SetColor(1, 1, 1, 1);
+	m_TestObject->SetLocation(0, 0, 0);
+	m_TestObject->SetSize(10, 10, 10);
+
+	//Init Objects
+	for (int i = 0; i < MAX_OBJECTS; i++)
+	{
+		m_Object[i] = NULL;
+	}
+
+	//Add Test Object
+	m_TestIdx = AddObjects(0, 0, 0, 100, 10, 10, 1, 1, 1, 1);
+	for (int i = 0; i < MAX_OBJECTS; i++)
+	{
+		m_TestIdxArray[i] = AddObjects(0, 0, 0, 100, 10, 10, 1, 1, 1, 1);
 	}
 }
 
@@ -25,5 +45,59 @@ void ScnMgr::RenderScene()
 	glClearColor(0.0f, 0.3f, 0.3f, 1.0f);
 
 	// Renderer Test
-	m_Renderer->DrawSolidRect(0, 0, 0, 4, 1, 0, 1, 1);
+	for (int i = 0; i < MAX_OBJECTS; i++)
+	{
+		float x, y, z = 0.f;
+		float sx, sy, sz = 0.f;
+		float r, g, b, a = 0.f;
+
+		m_Object[m_TestIdx]->GetLocation(&x, &y, &z);
+		m_Object[m_TestIdx]->GetSize(&sx, &sy, &sz);
+		m_Object[m_TestIdx]->GetColor(&r, &g, &b, &a);
+
+		m_Renderer->DrawSolidRect(x, y, z, sz, r, g, b, a);
+	}
+}
+
+int ScnMgr::AddObjects(float x, float y, float z, float sx, float sy, float sz, float r, float g, float b, float a)
+{
+	//Search empty slot
+	int idx = -1;
+	for (int i = 0; i < MAX_OBJECTS; i++)
+	{
+		if (m_Object[i] == NULL)
+		{
+			idx = i;
+			break;
+		}
+	}
+	if (idx == -1)
+	{
+		std::cout << "No more remaining object. \n";
+		return -1;
+	}
+	m_Object[idx] = new Object();
+	m_Object[idx]->SetColor(r, g, b, a);
+	m_Object[idx]->SetLocation(x, y, z);
+	m_Object[idx]->SetSize(sx, sy, sz);
+
+	return idx;
+}
+void ScnMgr::DeleteObject(int idx)
+{
+	if (idx < 0)
+	{
+		std::cout << "Negative idx does not allowed. \n";
+		return;
+	}
+	if (idx >= MAX_OBJECTS)
+	{
+		std::cout << "Requested idx exceeds MAX_OBJECTS count. \n";
+		return;
+	}
+	if (m_Object[idx] != NULL)
+	{
+		delete m_Object[idx];
+		m_Object[idx] = NULL;
+	}
 }
--- 게임소프트웨어공학/SimpleGame/SimpleGame/ScnMgr.h
+++ 게임소프트웨어공학/SimpleGame/SimpleGame/ScnMgr.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Renderer.h"
+#include "Object.h"
+#include "Globals.h"
 class ScnMgr
 {
 public:
@@ -7,8 +9,14 @@ public:
 	~ScnMgr();
 
 	void RenderScene();
+	int AddObjects(float x, float y, float z, float sx, float sy, float sz, float r, float g, float b, float a);
+	void DeleteObject(int idx);
 
 private:
 	Renderer * m_Renderer = NULL;
+	Object * m_TestObject = NULL;
+	int m_TestIdx = -1;
+	int m_TestIdxArray[MAX_OBJECTS];
+	Object * m_Object[MAX_OBJECTS];
 };
 
